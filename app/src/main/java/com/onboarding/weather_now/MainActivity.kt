2c3217e17726434a95ced8405f4d23c30901f1c2
Facundo Martinez <martinez.facundo@globant.com>
2021-08-03 14:33:11 -0300

    first commit

---
+++ app/src/main/java/com/onboarding/weather_now/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.onboarding.weather_now
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
